8b6621d2c88232b789679608dc51f24010ab524c
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-07 03:26:31 +0530

    Checking for valid phone Number !

---
+++ src/com/bridgelabz/regex/userreg/UserReg.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.regex.userreg;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserReg {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to user registration regex problem!");
+        Scanner scanner = new Scanner(System.in);
+        boolean validOutput = false;
+        UserRegUtility userRegUtility = new UserRegUtility();
+        System.out.println();
+        while ( validOutput == false ){
+            System.out.println("Enter first name : ");
+            String firstName = scanner.nextLine();
+            System.out.println("Enter last name : ");
+            String lastName = scanner.nextLine();
+            System.out.println("Enter Email : ");
+            String emailId = scanner.nextLine();
+            System.out.println("Enter Phone number : ");
+            String phoneNumber = scanner.nextLine();
+
+
+            userRegUtility.validateFirstName(firstName);
+            userRegUtility.validateLastName(lastName);
+            userRegUtility.validateEmail(emailId);
+            userRegUtility.validatePhoneNumber(phoneNumber);
+            break;
+        }
+
+    }
+}
