2adfe5b63cf52168ae9ea23cf47cbac60bb96579
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-07 02:10:44 +0530

    Checking for valid email id !

--- src/com/bridgelabz/regex/UserReg.java
+++ src/com/bridgelabz/regex/UserReg.java
@@ -6,6 +6,9 @@ import java.util.regex.Pattern;
 
 public class UserReg {
     static Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+    static Pattern emailPattern = Pattern.compile("^(abc.)[a-z]{3,}@bl.co.[a-z]{2}$");
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to user registration regex problem!");
         Scanner scanner = new Scanner(System.in);
@@ -13,12 +16,15 @@ public class UserReg {
         String firstName = scanner.nextLine();
         System.out.println("Enter last name starting with capital letter of at-least 3 character : \n");
         String lastName = scanner.nextLine();
+        System.out.println("Enter email :\n");
+        String email = scanner.nextLine();
         Matcher matcher1 = pattern.matcher(firstName);
         Matcher matcher2 = pattern.matcher(lastName);
-        if ( matcher1.matches() && matcher2.matches()){
-            System.out.println("First and last name is valid!");
+        Matcher matcher3 = emailPattern.matcher(email);
+        if ( matcher1.matches() && matcher2.matches() && matcher3.matches()){
+            System.out.println("Name and email is valid!");
         }else {
-            System.out.println("First and last name is invalid!");
+            System.out.println("Name and email is invalid!");
         }
 
     }
